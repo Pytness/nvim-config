287c23e10973184c445344139d81deb0bde98fa2
Fernando Vega <fvega@isid.es>
2025-05-08 14:37:11 +0200

    feat(copilot): use gpt 4o

--- plugins/user/copilot.lua
+++ plugins/user/copilot.lua
@@ -5,6 +5,7 @@ return {
   event = 'InsertEnter',
   config = function()
     require('copilot').setup {
+      copilot_model = 'gpt-4o-copilot',
       suggestion = {
         auto_trigger = true,
         accept = '<M-l>',
