20f53fb2c0f621758ee3be29dc13511b8008977d
Fernando Vega <fvega@isid.es>
2025-06-26 09:30:27 +0200

    feat: ignore e501 and e231, extend line length

--- lsp/pylsp.lua
+++ lsp/pylsp.lua
@@ -5,6 +5,11 @@ return {
       plugins = {
         pyls_black = { enabled = true },
         isort = { enabled = true },
+        pycodestyle = {
+          enabled = true,
+          ignore = { 'E501', 'E231' },
+          maxLineLength = 120,
+        },
       },
     },
   },
