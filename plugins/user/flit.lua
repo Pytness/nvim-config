a4d14f77107e2cd289b4b441637644383f3cc43c
Fernando Vega <fvega@isid.es>
2025-07-17 13:30:04 +0200

    refactor: move config function to opts

--- plugins/user/flit.lua
+++ plugins/user/flit.lua
@@ -1,12 +1,9 @@
 -- better f, F, t, T
 return {
   'ggandor/flit.nvim',
-  config = function()
-    require('flit').setup {
-      keys = { f = 'f', F = 'F', t = 't', T = 'T' },
-      labeled_modes = 'v',
-      multiline = true,
-      opts = {},
-    }
-  end,
+  opts = {
+    keys = { f = 'f', F = 'F', t = 't', T = 'T' },
+    labeled_modes = 'v',
+    multiline = true,
+  },
 }
