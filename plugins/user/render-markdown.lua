7c6d7c2b0e32683200762f493a349d828724e794
Fernando Vega <fvega@isid.es>
2025-07-29 15:47:11 +0200

    feat: add filetypes to render-markdown plugin

--- plugins/user/render-markdown.lua
+++ plugins/user/render-markdown.lua
@@ -1,8 +1,17 @@
 return {
   -- enabled = false,
   'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'echasnovski/mini.nvim',
+  },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
-  opts = {},
+  opts = {
+    file_types = {
+      'markdown',
+      'md',
+      'copilot-chat',
+    },
+  },
 }
