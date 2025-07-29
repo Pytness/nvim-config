bb17d2638361abe4146942cfbfb535722738540f
Fernando Vega <fvega@isid.es>
2025-07-29 12:56:00 +0200

    feat: use cdmill/focus.nvim

--- keymaps/zen.lua
+++ keymaps/zen.lua
@@ -1,9 +1,39 @@
+function focus(data)
+  local cursor = vim.api.nvim_win_get_cursor(0)
+
+  local start_line = cursor[1]
+  local end_line = vim.fn.line 'v'
+
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+
+  local line_count = end_line - start_line + 1
+
+  -- exit visual mode with feedkeys
+  local esc = vim.api.nvim_replace_termcodes('<Esc>', true, false, true)
+  vim.api.nvim_feedkeys(esc, 'n', true)
+
+  print('Focus lines:', start_line, end_line)
+  print('Line count:', line_count)
+
+  require('focus').toggle {
+    line1 = start_line,
+    line2 = end_line,
+
+    window = {
+      -- Set the cursor to the first line of the focus window
+      -- The focus window includes the first and last line of the buffer
+      -- as folds, so we add 2 to the height as padding
+      height = line_count + 2,
+    },
+
+    on_open = function(win)
+      vim.api.nvim_win_set_cursor(win, { start_line, 0 })
+    end,
+  }
+end
+
 return {
-  { 'n', '<leader>zn', ':TZNarrow<CR>', { desc = 'Narrow focus' } },
-  { 'v', '<leader>zn', ":'<,'>TZNarrow<CR>", { desc = 'Narrow focus' } },
-  { 'n', '<leader>za', ':TZAtaraxis<CR>', { desc = 'Zen mod' } },
+  { 'v', '<leader>F', focus, { desc = 'Narrow focus' } },
 }
-
--- vim.api.nvim_set_keymap('n', '<leader>zf', ':TZFocus<CR>', {})
-
--- vim.api.nvim_set_keymap('n', '<leader>zm', ':TZMinimalist<CR>', {})
--- plugins/user/zen.lua
+++ plugins/user/zen.lua
@@ -1,9 +1,45 @@
 return {
-  'Pocco81/true-zen.nvim',
-  config = function()
-    require('true-zen').setup {
-      -- your config goes here
-      -- or just leave it empty :)
-    }
-  end,
+  'cdmill/focus.nvim',
+  cmd = { 'Focus', 'Zen', 'Narrow' },
+  opts = {
+    border = 'none',
+    zindex = 40, -- zindex of the focus window. Should be less than 50, which is the float default
+    window = {
+      backdrop = 0.9, -- shade the backdrop of the focus window. Set to 1 to keep the same as Normal
+      -- height and width can be:
+      -- * an absolute number of cells when > 1
+      -- * a percentage of the width / height of the editor when <= 1
+      width = 120, -- width of the focus window
+      height = 11, -- height of the focus window
+      -- by default, no options are changed in for the focus window
+      -- add any vim.wo options you want to apply
+      options = {},
+    },
+    zen = {
+      opts = {
+        cmdheight = 0, -- disable cmdline
+        cursorline = false, -- disable cursorline
+        laststatus = 0, -- disable statusline
+        number = false, -- disable number column
+        relativenumber = false, -- disable relative numbers
+        foldcolumn = '0', -- disable fold column
+        signcolumn = 'no', -- disable signcolumn
+        statuscolumn = ' ', -- disable status column
+      },
+      diagnostics = false, -- disables diagnostics
+    },
+    plugins = {
+      -- uncomment any of the lines below to disable that option in Focus mode
+      -- options = {
+      --   disable some global vim options (vim.o...) e.g.
+      --   ruler = false
+      -- },
+      -- twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+      -- gitsigns = { enabled = false }, -- disables git signs
+      -- tmux = { enabled = false }, -- disables the tmux statusline
+      -- diagnostics = { enabled = false }, -- disables diagnostics
+      -- todo = { enabled = false }, -- if set to "true", todo-comments.nvim highlights will be disabled
+    },
+    -- callback where you can add custom code when the focus window opens
+  },
 }
