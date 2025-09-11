3444b27f960899d4727b4c54ec4cf55de26342e4
Fernando Vega <fvega@isid.es>
2025-09-11 15:44:29 +0200

    feat: add lsp related search keymaps

--- keymaps/search.lua
+++ keymaps/search.lua
@@ -42,6 +42,8 @@ return {
   { 'n', '<leader>slS', fzf_factory 'lsp_workspace_symbols', { desc = 'LSP symbols in workspace' } },
   { 'n', '<leader>sld', fzf_factory 'diagnostics_document', { desc = 'Search diagnostics' } },
   { 'n', '<leader>slD', fzf_factory 'diagnostics_workspace', { desc = 'Search diagnostics in workspace' } },
+  { 'n', '<leader>slr', fzf_factory 'lsp_references', { desc = 'LSP references' } },
+  { 'n', '<leader>sll', fzf_factory 'lsp_finder', { desc = 'LSP finder' } },
 
   { 'n', '<leader>s/', fzf_factory 'lines', { desc = 'Search in open buffers' } },
 
