0e2ea09baa88298244efd5a91333140f6204abb1
Fernando Vega <pytness@gmail.com>
2025-10-13 06:15:52 +0200

    fix: deprecation of flit.nvim

--- plugins/user/leap.lua
+++ plugins/user/leap.lua
@@ -1,10 +1,16 @@
 -- Allows you to jump to any character in the visible area of the screen
 return {
   'ggandor/leap.nvim',
-  config = function()
-    require('leap').setup {
-      safe_labels = '', -- Disable the default labels so keypresses are always 3
-      labels = 'sfnjklhodweimbuyvrgtaqpcxz',
-    }
+
+  opts = {
+    safe_labels = '', -- Disable the default labels so keypresses are always 3
+    labels = 'sfnjklhodweimbuyvrgtaqpcxz',
+  },
+
+  config = function(_, opts)
+    require('leap.user').set_repeat_keys(';', ',', {
+      relative_directions = true,
+      modes = { 'n', 'x', 'o' },
+    })
   end,
 }
