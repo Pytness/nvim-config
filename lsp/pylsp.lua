f92b5c6399afee9577fcf02bd51acd901e6a681f
Fernando Vega <pytness@gmail.com>
2025-10-13 06:22:45 +0200

    chore(lsp): remove flake and mccabe, extend ignore

--- lsp/pylsp.lua
+++ lsp/pylsp.lua
@@ -5,9 +5,19 @@ return {
       plugins = {
         pyls_black = { enabled = true },
         isort = { enabled = true },
+        flake8 = { enabled = false },
+        mccabe = { enabled = false },
         pycodestyle = {
           enabled = true,
-          ignore = { 'E501', 'E231' },
+          ignore = {
+            'E501',
+            'E231',
+            -- Recommended by Black
+            'E203',
+            'E701',
+            'E704',
+            'W503',
+          },
           maxLineLength = 120,
         },
       },
