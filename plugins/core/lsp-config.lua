0d50f9a54a928de635a0bcf41c1f7c6b921a8213
Fernando Vega <pytness@gmail.com>
2025-11-06 09:35:35 +0100

    chore: remove lsp-config in favor of mason-lspconfig

--- plugins/core/lsp-config.lua
+++ plugins/core/lsp-config.lua
@@ -1,51 +1,39 @@
+local ensure_installed = {
+  'lua_ls',
+  'stylua',
+  'pylsp',
+  'rust_analyzer',
+  'omnisharp',
+  'clangd',
+}
+
+-- Aditional server to enable (installed servers are enabled automatically)
+local enabled_servers = {
+  'gdscript',
+}
+
 -- LSP Configuration & Plugins
 return {
-  'neovim/nvim-lspconfig',
-  event = { 'BufReadPost', 'BufNewFile' },
-  cmd = { 'LspInfo', 'LspInstall', 'LspUninstall' },
-  dependencies = {
-    -- Automatically install LSPs and related tools to stdpath for neovim
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    'saghen/blink.cmp',
+  'mason-org/mason-lspconfig.nvim',
 
-    -- Useful status updates for LSP.
-    { 'j-hui/fidget.nvim', opts = {} },
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    { 'mason-org/mason.nvim', opts = {} },
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
   },
 
+  opts = {},
+
   config = function(_, opts)
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
-      callback = function(event)
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-
-        if client and client.server_capabilities.documentHighlightProvider then
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.document_highlight,
-          })
-
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.clear_references,
-          })
-        end
-      end,
-    })
-
-    require('mason').setup()
-
-    local ensure_installed = {
-      'lua_ls',
-      'stylua',
-      'pylsp',
-      'rust_analyzer',
-      'omnisharp',
-      'clangd',
-    }
-
+    require('mason-lspconfig').setup(opts)
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+    for _, server in ipairs(ensure_installed) do
+      vim.lsp.enable(server)
+    end
+
+    for _, server in ipairs(enabled_servers) do
+      vim.lsp.enable(server)
+    end
   end,
 }
