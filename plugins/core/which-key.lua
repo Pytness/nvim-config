baa03acdafaf913ea0f9df84115256e637229b7b
Fernando Vega <pytness@gmail.com>
2026-02-27 08:16:00 +0100

    fix: i_ctrl_o mappings not working
    Forked folke's and applied fix

--- plugins/core/which-key.lua
+++ plugins/core/which-key.lua
@@ -1,6 +1,6 @@
 -- Useful plugin to show you pending keybinds.
 return {
-  'folke/which-key.nvim',
+  'pytness/which-key.nvim',
   event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
   opts = {
     preset = 'modern',
