8bf4485729b15083639474661749cfef43cc9088
Fernando Vega <pytness@gmail.com>
2026-05-31 19:14:12 +0200

    feat: remap <f12> to 'q'

--- init.lua
+++ init.lua
@@ -5,6 +5,10 @@ package.path = nvim_dir .. '?.lua;' .. package.path
 package.path = nvim_dir .. '?/?.lua;' .. package.path
 package.path = nvim_dir .. '?/init.lua;' .. package.path
 
+-- remap <f12> to 'q'
+vim.api.nvim_set_keymap('n', '<F12>', 'q', { noremap = true, silent = true })
+vim.keymap.set('n', 'q', '<nop>', { noremap = true, silent = true })
+
 require 'custom.keymap_stats'
 require 'libs'
 require 'options'
